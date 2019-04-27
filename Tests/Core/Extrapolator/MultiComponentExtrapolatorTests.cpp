605bb7235f9efbad6c35337a688ae8c756e2fbc8
Jin Zhang <jinz@lxplus630.cern.ch>
2019-04-27 16:31:24 +0200

    surface collector and  multi & single check

--- Tests/Core/Extrapolator/MultiComponentExtrapolatorTests.cpp
+++ Tests/Core/Extrapolator/MultiComponentExtrapolatorTests.cpp
@@ -54,15 +54,25 @@ namespace Test {
 
   // Get the navigator and provide the TrackingGeometry
   Navigator navigator(tGeometry);
+  Navigator multi_navigator(tGeometry);
 
   using BFieldType          = ConstantBField;
   using EigenStepperType    = MultiEigenStepper<BFieldType>;
   using EigenPropagatorType = Propagator<EigenStepperType, Navigator>;
+  using MultiEigenStepperType    = MultiEigenStepper<BFieldType>;
+  using MultiEigenPropagatorType = Propagator<MultiEigenStepperType, Navigator>;
 
   const double        Bz = 2. * units::_T;
   BFieldType          bField(0, 0, Bz);
+
+  // define the scs
   EigenStepperType    estepper(bField);
   EigenPropagatorType epropagator(std::move(estepper), std::move(navigator));
+
+  // define the mcs
+  MultiEigenStepperType    multi_estepper(bField);
+  MultiEigenPropagatorType multi_epropagator(std::move(multi_estepper),
+                                             std::move(multi_navigator));
 
   const int ntests    = 100;
   bool      debugMode = true;
@@ -135,12 +145,87 @@ namespace Test {
     options.maxStepSize = 10. * units::_cm;
     options.pathLimit   = 25 * units::_cm;
 
-    const auto& result = epropagator.propagate(start, options).value();
+    const auto& result = multi_epropagator.propagate(start, options).value();
     if (debugMode) {
       const auto& output = result.get<DebugOutput::result_type>();
       std::cout << ">>> Extrapolation output " << std::endl;
       std::cout << output.debugString << std::endl;
     }
+  }
+
+  // This test case checks that no segmentation fault appears
+  // - this tests the same surfaceHit of different stepper
+  BOOST_DATA_TEST_CASE(
+      test_surface_collection_,
+      bdata::random((bdata::seed = 10,
+                     bdata::distribution
+                     = std::uniform_real_distribution<>(0.4 * units::_GeV,
+                                                        10. * units::_GeV)))
+          ^ bdata::random((bdata::seed = 11,
+                           bdata::distribution
+                           = std::uniform_real_distribution<>(-M_PI, M_PI)))
+          ^ bdata::random((bdata::seed = 12,
+                           bdata::distribution
+                           = std::uniform_real_distribution<>(1.0, M_PI - 1.0)))
+          ^ bdata::random((bdata::seed = 13,
+                           bdata::distribution
+                           = std::uniform_int_distribution<>(0, 1)))
+          ^ bdata::xrange(ntests),
+      pT,
+      phi,
+      theta,
+      charge,
+      index)
+  {
+
+    double dcharge = -1 + 2 * charge;
+    (void)index;
+
+    // define start parameters
+    double   x  = 0;
+    double   y  = 0;
+    double   z  = 0;
+    double   px = pT * cos(phi);
+    double   py = pT * sin(phi);
+    double   pz = pT / tan(theta);
+    double   q  = dcharge;
+    Vector3D pos(x, y, z);
+    Vector3D mom(px, py, pz);
+    /// a covariance matrix to transport
+    ActsSymMatrixD<5> cov;
+    // take some major correlations (off-diagonals)
+    cov << 10 * units::_mm, 0, 0.123, 0, 0.5, 0, 10 * units::_mm, 0, 0.162, 0,
+        0.123, 0, 0.1, 0, 0, 0, 0.162, 0, 0.1, 0, 0.5, 0, 0, 0,
+        1. / (10 * units::_GeV);
+    auto covPtr = std::make_unique<const ActsSymMatrixD<5>>(cov);
+    CurvilinearParameters start(std::move(covPtr), pos, mom, q);
+
+    // A PlaneSelector for the SurfaceCollector
+    using PlaneCollector = SurfaceCollector<PlaneSelector>;
+
+    PropagatorOptions<ActionList<PlaneCollector>> options(
+        tgContext, mfContext);
+    options.maxStepSize       = 10. * units::_cm;
+    options.pathLimit         = 25 * units::_cm;
+    options.debug             = debugMode;
+
+    PropagatorOptions<ActionList<PlaneCollector>> multi_options(
+        tgContext, mfContext);
+    multi_options.maxStepSize = 10. * units::_cm;
+    multi_options.pathLimit   = 25 * units::_cm;
+    multi_options.debug       = debugMode;
+
+    // sigle component
+    const auto& result           = epropagator.propagate(start, options).value();
+    auto        collector_result = result.get<PlaneCollector::result_type>();
+
+    // multi component
+    const auto& multi_result 	 = multi_epropagator.propagate(start, multi_options).value();
+    auto multi_collector_result  = multi_result.get<PlaneCollector::result_type>();
+
+    BOOST_CHECK_EQUAL(collector_result.collected.size(),
+                      multi_collector_result.collected.size());
+    BOOST_CHECK(collector_result.collected == multi_collector_result.collected);
   }
 
 }  // namespace Test
